72b8493ad4b66bece4f9bcd7061dc17d195c567f
PrzemekPietrzyk <info@pp-web.eu>
2022-05-03 13:43:04 +0200

    revised struckture and files , compatible with frontend

--- src/backend/controllers/users.js
+++ src/backend/controllers/users.js
@@ -1,44 +1,45 @@
 const router = express.Router();
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+
 const User = require('../models/user')
 
-
-exports.userPost = async  (req, res) => {
+app.post('/create/user', async  (req, res) => {
     const { username ,email , password } = req.body;
-    try
-    {
-       mongoose.connect(URL_DB);
+    console.log(req.body);
+    try{
+        mongoose.connect(URL_DB);
         const user = new User({
-            username:username,
-            email:email,
+            username: username,
+            email: email,
             password: createHash(password) ,
-            token:signAccessToken(email)
+            token: signAccessToken(email)
         });
         const x = await user.save();
         res.status(200).json({
             success: true,
-            message:'user created success',
+            message: 'user created success',
         });
 
-    }
-    catch(err)
-    {
+    }catch(err){
         res.status(400).json({
-            message:'error in post,"/create/user" ',
+            message: 'error in post,"/create/user" ',
             error:err
         });
     };
-}
+ 
+});
 
-exports.userLogin = async  (req, res) => {
+app.post('/login', async  (req, res) => {
     
-    const { email, password } = req.body;
+    const { 
+        email,
+        password  
+        } = req.body;
 
-    try
-    {
-       mongoose.connect(URL_DB);
-        const email1 = await User.findOne({ email:email });
+    try{
+        mongoose.connect(URL_DB);
+        const email1 = await User.findOne({email:email});
        if(email1.password === createHash(password) )
        {
            res.cookie( "cookie_Token",email1.token,
@@ -50,31 +51,29 @@ exports.userLogin = async  (req, res) => {
                token:email1.token,
                user:email1
            })
-       }
-       else
-       {
+       }else{
+
             res.clearCookie("cookie_Token").status(200).json({
                 success: true,
-                message:'password or email is invalid',
-                token:'null',
-                user:'null'
+                message: 'password or email is invalid',
+                token: 'null',
+                user: 'null'
             })
-       }     
+       }
+        
         res.end();
-    }
-    catch (err)
-    {
+    }catch (err) {
         res.clearCookie("cookie_Token").status(200).json({
-            message:'error in post,"/login',
-            message:'password or email is invalid',
-            token:'null',
-            user:'null',
+            message: 'error in post," /login " ',
+            message: 'password or email is invalid',
+            token: 'null',
+            user: 'null',
             error:err
         })
     };
-}
+});
 
-exports.userUpdate = async (req, res) =>
+app.post('/user/update', async (req, res) =>
 {
     const { username , email , password , confirmEmail} = req.body;
 
@@ -85,17 +84,16 @@ exports.userUpdate = async (req, res) =>
         res.status(200).json({success:true});
         res.end();
 
-    }
-    else
-    {
+    }else{
         mongoose.connect(URL_DB);
         const email1 = await User.updateOne({email:confirmEmail},{$set: {username:username, password:createHash(password),email:email} });
         res.status(200).json({success:true});
         res.end();
     }
-}
+    
+});
 
-exports.userLogout = (req, res) =>
+app.post('/user/logout', (req, res) =>
 {
     res.clearCookie("cookie_Token")
     .status(200)
@@ -103,17 +101,72 @@ exports.userLogout = (req, res) =>
         success: true,
         message: 'user logged out'
     });
-}
+});
 
-exports.userDelete = (req, res) =>
+app.post('/img',async (req, res)=>{
+    const { email } = req.body
+    mongoose.connect(URL_DB);
+    const img = await User.findOne({email:email});
+  
+    res.status(200).send({
+            img:img.img
+    })
+});
+
+app.post('/uploadphoto', upload.single("testImage"), async (req, res) =>
 {
-    const mongoId = mongoose.isValidObjectId(req.params.id);
-    if (!mongoId)
-        return res.status(400).send('wrong Id');
-    const userDeleted =  await User.findByIdAndDelete(req.params.id);
-    if (!userDeleted) 
-        return res.status(404).send('wrong Id');
-    res.status(200).send(`user with id ${req.params.id} deleted!`);
-}
+ 
+    const { buffer, originalname, mimetype , confirmEmail} = req.body;
+    
+    mongoose.connect(URL_DB);
+    
+    const newImage = new Image();
+    newImage.data = buffer;
+    newImage.name = Date.now() + "-" + originalname;
+    newImage.contentType = mimetype;
+    
+    const email1 = await User.updateOne({email:confirmEmail},{$set:{img:buffer} });
 
-mongoose.connection.close();
+    res.status(200).json({success:true});
+    res.end();
+});
+
+
+app.get('/images/:name', (req, res) =>
+{
+    
+    Image.findOne({ name: req.params.name }).then(image =>
+    {
+        
+        if(image)
+        {
+            console.log(image);
+
+            res.status(200)
+                .contentType(image.contentType)
+                .send(image.data);
+        }
+        else
+        {
+            res.status(404).send("Image not found!");
+        }
+    });
+});
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile( path.join(__dirname,'../../dist' , 'index.html'));
+});
+
+app.get('/Meeting-room', (req, res) => {
+    res.redirect(`/Meeting-room/${uuid()}`);
+    
+});
+
+app.get('/Meeting-room/:room', (req, res) => {
+  
+    res.send( { roomName: req.params.room })
+});
+
+// mongoose.connection.close();
+
+export { app }
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,30 +1,98 @@
 require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-const user = require('./routes/users');
-
+const express = require("express");
 const app = express();
-const URL_DB = process.env.DB;
-const port = process.env.PORT || 5000;
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const {uuid} = require('uuidv4');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const multer = require('multer');
 
+import { app } from './controllers/users.js';
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+      origin: "https://chatt-test.herokuapp.com/",
+      methods: ["GET", "POST"],
+    },
+});
+
+const URL_DB = process.env.DB;
+const port = process.env.PORT || 3000;
 const SECRET_TOKEN = process.env.SECRET_TOKEN;
-const signAccessToken = (data) => {
-    return jwt.sign(data,SECRET_TOKEN);
+const signAccessToken = ( data ) =>
+{
+    return jwt.sign( data, SECRET_TOKEN );
 }
 
-const createHash = (password) =>
+const createHash = ( password ) =>
 {
-    const secret = SECRET_KEY;
+    const secret = process.env.SECRET_KEY;
     const hash = crypto.createHmac('sha256', secret).update(password).digest('hex');
     return hash;
 }
 
-app.use( '/static' ,express.static(path.join(__dirname, '../../dist'), { index: false }));
+const upload = multer({
+    
+    limits:
+    {
+        fileSize: 90000000000
+    }
+});
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile( path.join(__dirname,'../../dist' , 'index.html'));
-})
+// ----------- MIDDLEWARE -----------------------------
+
+app.use(cors());
+app.use( '/static' ,express.static(path.join(__dirname, '../../dist'), { index: false }));
+app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header
+    (
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "*")
+    
+    next();
+});
+
+// ----------- socket. -----------------------------
+
+io.on('connection', (socket ) => 
+{
+    
+    console.info(`Client connected [id = ${socket.id}]`);
+
+    socket.on('room', (room) => 
+    {
+        console.log('user joint to : ' + room );
+        socket.join(room);
+    });
+
+    socket.on('message', ( msg ) =>
+    {
+        console.log(msg);
+        io.sockets.in(msg.RoomName).emit('message', { message: msg.massage,username:msg.username});
+    });
+
+    socket.on('disconnect',() => 
+    
+    {
+        console.info(`Client gone [id = ${socket.id}]`);
+    });
+
+});
+
+// ------------------------------
 
 app.listen(port,()=>{
     console.log( `server listening on ${port}` );
